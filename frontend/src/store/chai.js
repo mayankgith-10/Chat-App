65cbf87831fab5feca5c3561f061a99767f4c201
Mayank <00mayanks11@gmail.com>
2026-02-20 12:28:59 +0530

    chat-app

---
+++ frontend/src/store/chai.js
@@ -0,0 +1,24 @@
+// const chai = [1, 2, 3, 4, 5];
+
+// const myNums = chai.map( (item) => {return item * 2});
+
+// console.log(myNums);
+
+// const final = chai.reduce( (acc, currVal) => (acc + currVal));
+
+// console.log(final);
+
+// const fruits = ["apple", "mango", "banana", "orange", "lichi"];
+
+// const jiFuits = fruits.map( (fruit) => (fruit + " ji"));
+
+// console.log(jiFuits);
+
+const obj = {
+    name: "Mayank",
+    surname: "soni",
+    age: 23,
+    maritial_status: "unmarried",
+}
+
+console.log(obj.surname);
