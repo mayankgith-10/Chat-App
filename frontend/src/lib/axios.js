95bb275c38d787cc5f9d85ddf1dd6074dcced8a5
Mayank Soni <00mayanks11@gmail.com>
2026-04-06 07:14:22 +0530

    Change axios base URL to production endpoint

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: "https://chat-app-backend-6pms.onrender.com/api",
     withCredentials: true,
 })
